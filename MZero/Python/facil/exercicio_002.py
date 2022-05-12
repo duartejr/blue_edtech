ae73a03534a6dc995fe0b6e6a4ed7cdf11982f84
Duarte Junior <duarte.jr105@gmail.com>
2022-05-12 20:45:23 -0300

    Reordenando repositorio

--- MZero/Python/facil/exercicio_002.py
+++
@@ -1,5 +0,0 @@
-# Elabore um programa que escreve seu nome completo na primeira linha, seu 
-# endereço na segunda e o CEP e telefone na terceira.
-print("""Duarte Junior
-Rua Sem Nome, sn, Redenção - Ceará
-CEP: 62790-000""")
